4cfe97dfe5b0f278ce21350dadf17a15f9f2adce
Naderab <abdellaouinader@gmail.com>
2024-11-18 08:42:56 +0100

    Manipulation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ResidencesComponent } from './Components/residences/residences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ResidencesComponent
   ],
   imports: [
     BrowserModule,
