6dd08f2e4017e029408e8a57d5d841df4559f09d
Naderab <abdellaouinader@gmail.com>
2024-11-22 16:40:24 +0100

    Manipulation des composants part2

--- src/app/Components/residences/residences.component.ts
+++ src/app/Components/residences/residences.component.ts
@@ -7,6 +7,9 @@ import { Residence } from '../../models/residence';
 })
 export class ResidencesComponent {
   title: string = 'Liste des résidences';
+  selectedResidence: Residence = new Residence();
+  hide: boolean = true;
+  searchText: string = ''
   residences: Residence[] = [
     {
       id: 1,
@@ -37,4 +40,32 @@ export class ResidencesComponent {
       status: 'En Construction',
     },
   ];
+
+  showLocation(r: Residence) {
+    this.hide = false;
+    this.selectedResidence = r;
+  }
+
+  filterByAdsress() {
+    return this.residences.filter(
+      (p) =>
+        p.address.toLowerCase()
+          .includes(this.searchText.toLowerCase()))
+  }
+
+  listFavoris: Residence[] = []
+  
+  addToFavoris(r:Residence) {
+    let index = this.listFavoris.findIndex((p) => p.id == r.id);
+    if (index == -1) {
+      this.listFavoris.push(r)
+    }
+    else {
+      this.listFavoris.splice(index,1)
+    }
+  }
+
+  isFavoris(r: Residence) {
+    return this.listFavoris.some((p)=>p.id == r.id)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { ResidencesComponent } from './Components/residences/residences.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ResidencesComponent } from './Components/residences/residences.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
