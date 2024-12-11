9e14c2407f8fe5cfd826d5afec24dd972c8dfbaf
Naderab <abdellaouinader@gmail.com>
2024-12-11 19:11:23 +0100

    Form

--- src/app/Components/residences/add-residence/add-residence.component.ts
+++ src/app/Components/residences/add-residence/add-residence.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-residence',
   templateUrl: './add-residence.component.html',
-  styleUrls: ['./add-residence.component.css']
+  styleUrls: ['./add-residence.component.css'],
 })
 export class AddResidenceComponent {
 
+  residence: FormGroup = new FormGroup({
+    //name: new FormControl({value:'résidence name',disabled:true}, []),
+    name:new FormControl('',[Validators.required,Validators.minLength(3)]),
+    address: new FormControl('', [Validators.required]),
+    /*address: new FormGroup({
+      street: new FormControl('', []),
+      city:new FormControl('',[])
+    }),*/
+    image: new FormControl('', [Validators.required]),
+    status: new FormControl('Disponible', [])
+  })
+ 
+  get name() {
+    return this.residence.get('name');
+  }
+  get address() {
+    return this.residence.get('address')
+  }
+  get image() {
+    return this.residence.get('image')
+  }
+
+  add() {
+    console.log(this.residence.value);
+    console.log(this.residence.getRawValue());
+    this.residence.patchValue({
+      name:'test'
+    })
+    console.log(this.residence.getRawValue());
+    this.residence.reset()
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ResidencesComponent } from './Components/residences/residences.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { ResidenceDetailsComponent } from './Components/residences/residence-details/residence-details.component';
+import { AddResidenceComponent } from './Components/residences/add-residence/add-residence.component';
 
 const routes: Routes = [
   { path: 'residences', component: ResidencesComponent },
   { path: '', redirectTo: '/residences', pathMatch: 'full' },
-  {path:'details/:id',component:ResidenceDetailsComponent},
-  {path:'**',component:NotFoundComponent}
+  { path: 'details/:id', component: ResidenceDetailsComponent },
+  { path: 'residence/add', component: AddResidenceComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
