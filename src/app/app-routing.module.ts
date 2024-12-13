a6bd288ac183ac5096632e49149e0a648a31dd83
Naderab <abdellaouinader@gmail.com>
2024-12-13 15:28:08 +0100

    Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { ResidencesComponent } from './Components/residences/residences.componen
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { ResidenceDetailsComponent } from './Components/residences/residence-details/residence-details.component';
 import { AddResidenceComponent } from './Components/residences/add-residence/add-residence.component';
+import { ApartmentsComponent } from './Components/Apartments/apartments/apartments.component';
+import { AddApartmentComponent } from './Components/Apartments/add-apartment/add-apartment.component';
+import { ApartmentsByResidenceComponent } from './Components/Apartments/apartments-by-residence/apartments-by-residence.component';
 
 const routes: Routes = [
   { path: 'residences', component: ResidencesComponent },
   { path: '', redirectTo: '/residences', pathMatch: 'full' },
   { path: 'details/:id', component: ResidenceDetailsComponent },
   { path: 'residence/add', component: AddResidenceComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: 'apartments', component: ApartmentsComponent },
+  { path: 'apartment/add/:idR', component: AddApartmentComponent },
+  { path: 'residence/show/:id', component: ApartmentsByResidenceComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
